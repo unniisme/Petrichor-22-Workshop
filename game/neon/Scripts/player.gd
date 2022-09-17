ee3582753676e6f9b55478839ae76f53a63ca044
unniisme <122001019@smail.iitpkd.ac.in>
2022-09-17 23:47:11 +0530

    Session 1, 2 and wrapper

--- game/neon/Scripts/player.gd
+++ game/neon/Scripts/player.gd
@@ -11,16 +11,20 @@ func _ready():
 	
 func _physics_process(delta):
 	
-	#player velocity
+	# player velocity
 	velocity.y += gravity*delta
 	
-	# 
+	# Capture input and update x velocity accordingly
 	velocity.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	velocity.x = velocity.x*movementSpeed
 	
+	# Jumping if player is on the floor
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y -= jumpSpeed
-	
+		
+	# Jump relese
+	if Input.is_action_just_released("Jump"):
+		velocity.y = 0 if velocity.y<0 else velocity.y
 	
 	#Function to move player
 	velocity = move_and_slide(velocity, Vector2.UP)
