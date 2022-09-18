ff62185933f62a0794feaf1976bcdf0e74635bd3
JerryJohnThomas <68646979+JerryJohnThomas@users.noreply.github.com>
2022-09-18 11:55:10 +0530

    Update currentCode.gd

--- currentCode.gd
+++ currentCode.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,4 +18,32 @@ func _ready():
 func _on_killer_area_body_entered(body):
 	if(body.name=="Player"):
 		get_tree().reload_current_scene()
+	
+
+
+var score=0
+
+func _on_Coin_body_entered(body):
+	if(body.name=="Player"):
+		score=score+1
+		print("new _score : ", score)
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
